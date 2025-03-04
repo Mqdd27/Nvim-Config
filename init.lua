45c9059386e63010d3772d6d2cf3c2da5194a78a
Mqdd27 <ahmadkimpir@gmail.com>
2025-03-04 18:55:41 +0700

    Add line numbering etc

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 vim.g.mapleader = " " --map leader to space
 vim.g.maplocalleader = " " --map leader to space
+vim.o.number = true
+vim.o.ignorecase = false  -- Disable case-insensitive
+vim.o.smartcase = false   -- Disable smart case
+vim.opt.smartindent = true -- Smart Indent
 
 
 require("keymaps")
