b09ae38de9e00f1da3ac26a72dabb18ec22fb488
Mqdd27 <ahmadkimpir@gmail.com>
2025-03-15 05:45:29 +0700

    feat: enable case insensitive search

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = " " --map leader to space
 vim.g.maplocalleader = " " --map leader to space
 vim.o.number = true
-vim.o.ignorecase = false  -- Disable case-insensitive
-vim.o.smartcase = false   -- Disable smart case
+vim.o.ignorecase = true  -- search case-insensitive
+vim.o.smartcase = true   -- smart case
 vim.opt.smartindent = true -- Smart Indent
 vim.opt.shiftwidth = 4   -- Change indent width
 vim.opt.tabstop = 4      -- Indent width
